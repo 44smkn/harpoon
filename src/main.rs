e28c686ea0f50e2ba7951435829a9138ea98735e
Kenji Shimizu <jrywm121@gmail.com>
2019-07-28 03:02:51 +0900

    fix print for debug

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate serde;
 extern crate serde_json;
-use std::collections::VecDeque;
 use model::container_detail;
+use std::collections::VecDeque;
 
 fn main() {
     println!("Hello, harpoon!");
@@ -20,18 +20,33 @@ fn main() {
     {
         Ok(output) => (output.stdout, output.stderr),
         Err(e) => {
-            println!("failed. cause is {}", e);
+            println!("failed. cause:\n{}", e);
             std::process::exit(1);
         }
     };
-    println!(
-        "stdout: {}\nstderr: {}",
-        String::from_utf8_lossy(&stdout),
-        String::from_utf8_lossy(&stderr)
-    );
 
-    let details: Vec<container_detail::ContainerDetail> =
-        serde_json::from_str(&String::from_utf8(stdout).unwrap()).unwrap();
+    let (stdout, stderr) = match String::from_utf8(stdout) {
+        Ok(value) => (value, String::from_utf8(stderr).unwrap()),
+        Err(e) => {
+            println!("failed to parse from Vec<u8> to utf8. cause:\n{}", e);
+            std::process::exit(1);
+        }
+    };
+
+    let command_result = if String::is_empty(&stderr) {
+        String::from(&stdout)
+    } else {
+        stderr
+    };
+    dbg!(command_result);
+
+    let details: Vec<container_detail::ContainerDetail> = match serde_json::from_str(&stdout) {
+        Ok(value) => value,
+        Err(e) => {
+            println!("failed to parse from json to object. cause:\n{}", e);
+            std::process::exit(1);
+        }
+    };
 
     println!("{:?}", details);
 }
