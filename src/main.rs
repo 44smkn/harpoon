3bfcfba8561547f6b996a9dda50e4c5b04c525b5
Kenji Shimizu <jrywm121@gmail.com>
2019-07-27 17:59:45 +0900

    handling loop process for container ids that is receeived from command line

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
+use std::collections::VecDeque;
+
 fn main() {
     println!("Hello, harpoon!");
 
-    let args = std::env::args().collect::<Vec<String>>();
+    let mut args = std::env::args().collect::<VecDeque<String>>();
+    let _ = args.pop_front();
+    if args.is_empty() {
+        println!("failed to parse args");
+        std::process::exit(1);
+    }
 
-    let (stdout, stderr) = match std::process::Command::new("docker")
-        .arg("inspect")
-        .arg(&args[1])
-        .output()
-    {
-        Ok(output) => (output.stdout, output.stderr),
-        Err(e) => {
-            println!("failed. cause is {}", e);
-            std::process::exit(1);
-        }
-    };
+    for id in args.iter() {
+        let (stdout, stderr) = match std::process::Command::new("docker")
+            .arg("inspect")
+            .arg(&id)
+            .output()
+        {
+            Ok(output) => (output.stdout, output.stderr),
+            Err(e) => {
+                println!("failed. cause is {}", e);
+                std::process::exit(1);
+            }
+        };
 
-    println!(
-        "stdout: {}\nstderr: {}",
-        String::from_utf8_lossy(&stdout),
-        String::from_utf8_lossy(&stderr)
-    );
+        println!(
+            "stdout: {}\nstderr: {}",
+            String::from_utf8_lossy(&stdout),
+            String::from_utf8_lossy(&stderr)
+        );
+    }
 }
