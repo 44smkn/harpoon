b0cbc9cb086d59769cf5731798c41994eca9b4ba
Kenji Shimizu <jrywm121@gmail.com>
2019-07-27 17:01:52 +0900

    execute external command

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,22 @@ fn main() {
 
     let args = std::env::args().collect::<Vec<String>>();
     println!("{:?}", args);
+
+    let (stdout, stderr) = match std::process::Command::new("docker")
+        .arg("inspect")
+        .arg("1435d5fbf3c6")
+        .output()
+    {
+        Ok(output) => (output.stdout, output.stderr),
+        Err(e) => {
+            println!("failed. cause is {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!(
+        "stdout: {:?}\nstderr: {}",
+        String::from_utf8_lossy(&stdout),
+        String::from_utf8_lossy(&stderr)
+    );
 }
