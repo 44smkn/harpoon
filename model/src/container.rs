f599be479b554566a995ee3f1e5bc2b775cdc3a4
Kenji Shimizu <jrywm121@gmail.com>
2019-07-29 23:45:17 +0900

    add annotation to struct for name of field

--- model/src/container.rs
+++ model/src/container.rs
@@ -1,38 +1,46 @@
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct Detail {
-    Path: String,
-    Args: Vec<String>,
-    State: ContainerState,
-    Mounts: Vec<Mount>,
-    Config: ContainerConfig,
+    path: String,
+    args: Vec<String>,
+    state: ContainerState,
+    mounts: Vec<Mount>,
+    config: ContainerConfig,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct ContainerState {
-    Status: String,
-    ExitCode: i32,
+    status: String,
+    exit_code: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct Mount {
-    Type: String,
-    Name: String,
-    Source: String,
-    Destination: String,
-    Driver: String,
-    RW: bool,
+    name: String,
+    source: String,
+    destination: String,
+    driver: String,
+
+    #[serde(rename = "Type")]
+    mount_type: String,
+
+    #[serde(rename = "RW")]
+    rw: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct ContainerConfig {
-    Env: Vec<String>,
-    Cmd: Vec<String>,
-    Image: String,
-    WorkingDir: String,
-    Entrypoint: Option<Vec<String>>,
-    Labels: HashMap<String, String>,
+    env: Vec<String>,
+    cmd: Vec<String>,
+    image: String,
+    working_dir: String,
+    entrypoint: Option<Vec<String>>,
+    labels: HashMap<String, String>,
 }
 
 pub enum Format {
@@ -53,22 +61,18 @@ impl Detail {
 
     pub fn details_to_string(fmt: Format, containers: Vec<Detail>) -> String {
         match fmt {
-            Format::Yaml => {
-                match serde_yaml::to_string(&containers) {
-                    Ok(value) => value,
-                    Err(e) => {
-                        println!("failed to serialize from object to yaml. cause:\n{}", e);
-                        std::process::exit(1);
-                    },
+            Format::Yaml => match serde_yaml::to_string(&containers) {
+                Ok(value) => value,
+                Err(e) => {
+                    println!("failed to serialize from object to yaml. cause:\n{}", e);
+                    std::process::exit(1);
                 }
             },
-            Format::Toml => {
-                match toml::to_string(&containers) {
-                    Ok(value) => value,
-                    Err(e) => {
-                        println!("failed to serialize from object to toml. cause:\n{}", e);
-                        std::process::exit(1);
-                    },
+            Format::Toml => match toml::to_string(&containers) {
+                Ok(value) => value,
+                Err(e) => {
+                    println!("failed to serialize from object to toml. cause:\n{}", e);
+                    std::process::exit(1);
                 }
             },
         }
