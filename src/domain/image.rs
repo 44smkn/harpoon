735f51e1548bf42352ad3bdc3d87a07fb8ba2266
Kenji Shimizu <jrywm121@gmail.com>
2020-12-18 23:35:07 +0900

    add getting image detail

--- src/domain/image.rs
+++ src/domain/image.rs
@@ -3,6 +3,12 @@ use chrono::{DateTime, Utc};
 use std::collections::HashMap;
 use std::error::Error;
 
+#[async_trait]
+pub trait ImageRepository {
+    async fn list(&self) -> Result<Vec<Image>, Box<dyn Error + Send + Sync>>;
+    async fn inspect(&self, id: String) -> Result<ImageDetail, Box<dyn Error + Send + Sync>>;
+}
+
 pub struct Image {
     pub id: String,
     pub parent_id: String,
@@ -13,7 +19,20 @@ pub struct Image {
     pub labels: HashMap<String, String>,
 }
 
-#[async_trait]
-pub trait ImageRepository {
-    async fn list(&self) -> Result<Vec<Image>, Box<dyn Error + Send + Sync>>;
+pub struct ImageDetail {
+    pub image: Image,
+    pub os: String,
+    pub architecture: String,
+    pub env: Vec<String>,
+    pub entrypoint: Vec<String>,
+    pub cmd: Vec<String>,
+    pub history: Vec<ImageRecord>,
+}
+
+pub type ImageHistory = Vec<ImageRecord>;
+
+pub struct ImageRecord {
+    pub id: String,
+    pub created_by: String,
+    pub size: i32,
 }
--- src/infrastructure/webapi/rest/image_repository.rs
+++ src/infrastructure/webapi/rest/image_repository.rs
@@ -46,6 +46,45 @@ where
 
         Ok(items)
     }
+
+    async fn inspect(
+        &self,
+        id: String,
+    ) -> Result<domain::ImageDetail, Box<dyn Error + Send + Sync>> {
+        let bytes = self.client.get(&format!("/images/{}/json", id)).await?;
+        let detail: ImageDetail = serde_json::from_slice(&bytes)?;
+        let bytes = self.client.get(&format!("/images/{}/history", id)).await?;
+        let records: ImageHistory = serde_json::from_slice(&bytes)?;
+        let mut items: domain::ImageHistory = Vec::new();
+        for record in records.into_iter() {
+            let item = domain::ImageRecord {
+                id: record.id,
+                created_by: record.created_by,
+                size: record.size,
+            };
+            items.push(item);
+        }
+
+        Ok(domain::ImageDetail {
+            image: domain::Image {
+                id: detail.id,
+                parent_id: detail.parent,
+                repo_tags: detail.repo_tags,
+                repo_digests: detail.repo_digests,
+                created: DateTime::<Utc>::from(
+                    DateTime::parse_from_rfc3339(&detail.created).unwrap(),
+                ),
+                size: detail.size,
+                labels: detail.container_config.labels.unwrap_or_else(HashMap::new),
+            },
+            os: detail.os,
+            architecture: detail.architecture,
+            env: detail.config.env,
+            entrypoint: detail.config.entrypoint,
+            cmd: detail.config.cmd,
+            history: items,
+        })
+    }
 }
 
 pub type ListImageOutput = Vec<Image>;
@@ -73,4 +112,170 @@ pub struct Image {
     pub labels: Option<HashMap<String, String>>,
     #[serde(rename = "Containers")]
     pub containers: i32,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ImageDetail {
+    #[serde(rename = "Id")]
+    pub id: String,
+    #[serde(rename = "Container")]
+    pub container: String,
+    #[serde(rename = "Comment")]
+    pub comment: String,
+    #[serde(rename = "Os")]
+    pub os: String,
+    #[serde(rename = "Architecture")]
+    pub architecture: String,
+    #[serde(rename = "Parent")]
+    pub parent: String,
+    #[serde(rename = "ContainerConfig")]
+    pub container_config: ContainerConfig,
+    #[serde(rename = "DockerVersion")]
+    pub docker_version: String,
+    #[serde(rename = "VirtualSize")]
+    pub virtual_size: i32,
+    #[serde(rename = "Size")]
+    pub size: i32,
+    #[serde(rename = "Author")]
+    pub author: String,
+    #[serde(rename = "Created")]
+    pub created: String,
+    #[serde(rename = "GraphDriver")]
+    pub graph_driver: GraphDriver,
+    #[serde(rename = "RepoDigests")]
+    pub repo_digests: Vec<String>,
+    #[serde(rename = "RepoTags")]
+    pub repo_tags: Vec<String>,
+    #[serde(rename = "Config")]
+    pub config: Config,
+    #[serde(rename = "RootFS")]
+    pub root_fs: RootFs,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContainerConfig {
+    #[serde(rename = "Tty")]
+    pub tty: bool,
+    #[serde(rename = "Hostname")]
+    pub hostname: String,
+    #[serde(rename = "Domainname")]
+    pub domainname: String,
+    #[serde(rename = "AttachStdout")]
+    pub attach_stdout: bool,
+    #[serde(rename = "PublishService")]
+    pub publish_service: String,
+    #[serde(rename = "AttachStdin")]
+    pub attach_stdin: bool,
+    #[serde(rename = "OpenStdin")]
+    pub open_stdin: bool,
+    #[serde(rename = "StdinOnce")]
+    pub stdin_once: bool,
+    #[serde(rename = "NetworkDisabled")]
+    pub network_disabled: bool,
+    #[serde(rename = "OnBuild")]
+    pub on_build: Vec<::serde_json::Value>,
+    #[serde(rename = "Image")]
+    pub image: String,
+    #[serde(rename = "User")]
+    pub user: String,
+    #[serde(rename = "WorkingDir")]
+    pub working_dir: String,
+    #[serde(rename = "MacAddress")]
+    pub mac_address: String,
+    #[serde(rename = "AttachStderr")]
+    pub attach_stderr: bool,
+    #[serde(rename = "Labels")]
+    pub labels: Option<HashMap<String, String>>,
+    #[serde(rename = "Env")]
+    pub env: Vec<String>,
+    #[serde(rename = "Cmd")]
+    pub cmd: Vec<String>,
+    #[serde(rename = "Entrypoint")]
+    pub entrypoint: Vec<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GraphDriver {
+    #[serde(rename = "Name")]
+    pub name: String,
+    #[serde(rename = "Data")]
+    pub data: Data,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Data {}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    #[serde(rename = "Image")]
+    pub image: String,
+    #[serde(rename = "NetworkDisabled")]
+    pub network_disabled: bool,
+    #[serde(rename = "OnBuild")]
+    pub on_build: Vec<::serde_json::Value>,
+    #[serde(rename = "StdinOnce")]
+    pub stdin_once: bool,
+    #[serde(rename = "PublishService")]
+    pub publish_service: String,
+    #[serde(rename = "AttachStdin")]
+    pub attach_stdin: bool,
+    #[serde(rename = "OpenStdin")]
+    pub open_stdin: bool,
+    #[serde(rename = "Domainname")]
+    pub domainname: String,
+    #[serde(rename = "AttachStdout")]
+    pub attach_stdout: bool,
+    #[serde(rename = "Tty")]
+    pub tty: bool,
+    #[serde(rename = "Hostname")]
+    pub hostname: String,
+    #[serde(rename = "Cmd")]
+    pub cmd: Vec<String>,
+    #[serde(rename = "Env")]
+    pub env: Vec<String>,
+    #[serde(rename = "Labels")]
+    pub labels: Option<HashMap<String, String>>,
+    #[serde(rename = "MacAddress")]
+    pub mac_address: String,
+    #[serde(rename = "AttachStderr")]
+    pub attach_stderr: bool,
+    #[serde(rename = "WorkingDir")]
+    pub working_dir: String,
+    #[serde(rename = "User")]
+    pub user: String,
+    #[serde(rename = "Entrypoint")]
+    pub entrypoint: Vec<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RootFs {
+    #[serde(rename = "Type")]
+    pub type_field: String,
+    #[serde(rename = "Layers")]
+    pub layers: Vec<String>,
+}
+
+pub type ImageHistory = Vec<ImageRecord>;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ImageRecord {
+    #[serde(rename = "Id")]
+    pub id: String,
+    #[serde(rename = "Created")]
+    pub created: i64,
+    #[serde(rename = "CreatedBy")]
+    pub created_by: String,
+    #[serde(rename = "Tags")]
+    pub tags: Vec<String>,
+    #[serde(rename = "Size")]
+    pub size: i32,
+    #[serde(rename = "Comment")]
+    pub comment: String,
 }
