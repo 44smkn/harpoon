d255d5cd3765200aedc3582c318f45b0ece6afd9
Kenji Shimizu <jrywm121@gmail.com>
2020-12-28 23:09:16 +0900

    inspect image

---
+++ src/usecase/inspect_image.rs
@@ -0,0 +1,22 @@
+use crate::domain::image::{ImageDetail, ImageRepository};
+use std::error::Error;
+
+pub struct InspectImageUsecase<T: ImageRepository> {
+    repository: T,
+}
+
+impl<T: ImageRepository> InspectImageUsecase<T> {
+    pub fn new(image_repository: T) -> Self {
+        InspectImageUsecase {
+            repository: image_repository,
+        }
+    }
+
+    pub async fn inspect_image(
+        &self,
+        id: impl Into<String>,
+    ) -> Result<ImageDetail, Box<dyn Error + Send + Sync>> {
+        let detail = self.repository.inspect(id).await?;
+        Ok(detail)
+    }
+}
