f9998084dd6297e4e176ab20e5251b66586984a4
Kenji Shimizu <jrywm121@gmail.com>
2021-01-12 22:54:56 +0900

    create usecase that gets image history

---
+++ src/usecase/get_image_history.rs
@@ -0,0 +1,22 @@
+use crate::domain::image::{ImageHistory, ImageRepository};
+use std::error::Error;
+
+pub struct GetImageHistoryUsecase<'a, T: ImageRepository> {
+    repository: &'a T,
+}
+
+impl<'a, T: ImageRepository> GetImageHistoryUsecase<'a, T> {
+    pub fn new(image_repository: &'a T) -> Self {
+        GetImageHistoryUsecase {
+            repository: image_repository,
+        }
+    }
+
+    pub async fn get_history(
+        &self,
+        id: String,
+    ) -> Result<ImageHistory, Box<dyn Error + Send + Sync>> {
+        let history = self.repository.history(id).await?;
+        Ok(history)
+    }
+}
--- src/usecase/mod.rs
+++ src/usecase/mod.rs
@@ -1,2 +1,3 @@
+pub mod get_image_history;
 pub mod inspect_image;
 pub mod list_image;
